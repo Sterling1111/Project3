5d669d3eeb5fcee9f4ce0d5a4f9a099af35f9f20
Sterling Jeppson <Sterlingijeppson@gmail.com>
2021-04-28 13:39:28 -0700

    implemented some of the home fragment

--- app/src/main/java/com/example/project3/Home_Fragment.java
+++ app/src/main/java/com/example/project3/Home_Fragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,7 +12,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 public class Home_Fragment extends Fragment {
+    ProgressBar calories_pb, protein_pb;
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_fragment, container,false);
+        View v = inflater.inflate(R.layout.home_fragment, container,false);
+
+        calories_pb = v.findViewById(R.id.calories_pb);
+        calories_pb.setProgress(25);
+
+        return v;
     }
 }
